b90b70f12484cafc0e0c311d9296eef5270457f7
tundeiness <tundeiness@yahoo.co.uk>
2020-09-22 22:28:13 +0100

    WIP header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import './App.css';
+import Header from './components/Header';
 
-function App() {
-  return (
-    <div className="App">
-      <p>Hello world!!</p>
+const App = () => (
+  <>
+    <Header />
+    <div className="container-fluid" id="main">
+      <div className="container">
+        <p>Hello World !!!</p>
+      </div>
     </div>
-  );
-}
+  </>
+);
 
 export default App;
