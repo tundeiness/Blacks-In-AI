c4e4e55c96fefc3855411242495adba81ae154a5
tundeiness <tundeiness@yahoo.co.uk>
2020-09-23 13:01:04 +0100

    introduction styling

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import Introduction from './components/Introduction';
 
 const App = () => (
   <>
     <Header />
     <div className="container-fluid" id="main">
-      <div className="container">
+      {/* <div className="container">
         <p>Hello World !!!</p>
-      </div>
+      </div> */}
+      <Introduction />
     </div>
   </>
 );
