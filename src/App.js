ceb41a187d4f0450f7f5b674b68398ca05ac21bc
tundeiness <tundeiness@yahoo.co.uk>
2020-09-24 11:35:54 +0100

    complete testimonial

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import './App.css';
 import Header from './components/Header';
 import Introduction from './components/Introduction';
