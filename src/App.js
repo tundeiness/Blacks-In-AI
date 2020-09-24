14098d0fc007fb89659002cac7229bbbcfd3503d
tundeiness <tundeiness@yahoo.co.uk>
2020-09-24 09:50:41 +0100

    install alice-carousel

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Header from './components/Header';
 import Introduction from './components/Introduction';
 import Membership from './components/membership';
+import Testimonial from './components/testimonial';
 
 const App = () => (
   <>
@@ -13,6 +14,7 @@ const App = () => (
       </div> */}
       <Introduction />
       <Membership />
+      <Testimonial />
     </div>
   </>
 );
--- src/components/testimonial.js
+++ src/components/testimonial.js
@@ -1,7 +1,13 @@
 import React from 'react';
 
 const Testimonial = () => (
-
+  <div className="container-fluid testimony-wrapper">
+    <div className="container testimony-card">
+      <div className="row">
+        <p>Content goes here</p>
+      </div>
+    </div>
+  </div>
 );
 
 export default Testimonial;
