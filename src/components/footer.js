961b4dac53ec96b8ad960b48d860093c7f518c3a
tundeiness <tundeiness@yahoo.co.uk>
2020-09-25 11:07:21 +0100

    create footer.js

---
+++ src/components/footer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const Footer = () => (
+  <div className="container-fluid call-wrapper">
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-4">
+          <p>Become a member</p>
+        </div>
+        <div className="col-lg-4" />
+        <div className="col-lg-4 ">
+          <a href="#" className="cta-button">Apply to Join</a>
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Footer;
