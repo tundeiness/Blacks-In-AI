837304f4f277bce1611b8913ef8f34b59390071c
tundeiness <tundeiness@yahoo.co.uk>
2020-09-26 14:44:57 +0100

    edit introduction component fonts and content spacing

--- src/components/Introduction.js
+++ src/components/Introduction.js
@@ -7,7 +7,7 @@ const Introduction = () => (
       <div className="row">
         <div className="col-lg-7 intro-statements">
           <h1>An intro sentence about membership.</h1>
-          <p>It has been placed here soley to demonstrate the look and feel of finished, typeset text and its only for show so she who searches for mening here will be sorely diasappointed.</p>
+          <p>It has been placed here soley to demonstrate the look and feel of finished, typeset text and its only for show so she who searches for meaning here will be sorely diasappointed.</p>
         </div>
         <div className="col-lg-5 intro-image">
           <img className="img-fluid" alt="Brand" src="https://user-images.githubusercontent.com/25479050/94007446-62585080-fd99-11ea-944a-7d3a3f405bdc.png" />
